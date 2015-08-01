9d60332a67c423fc920be6754117e9d8678c67ff
David Ford <david@smithandrobot.com>
2015-08-01 12:32:51 -0500

    more particles

--- src/js/particles/index.js
+++ src/js/particles/index.js
@@ -1,52 +1,80 @@
+'use strict'
 var backgroundFactory = require('./background');
 var background ;
 var particle = require('./particle');
 var vector = require('./vector');
 var utils = require('../utils');
-
+var speed = .25;
+var width = 1500;
+var height = 585;
 /*
  * Generates a particle array
- * @params {total} number number od particles to generate
+ * @params {total} number number of particles to generate
  * @returns {array} an array of particle instances
 */
 function generateParticles(total) {
   var particleArray = [];
   var i = 0;
-  var randomX;
-  var randomY
-  for(i; i<=total; total++) {
+
+  for(i; i<=total; i++) {
     particleArray.push(particle.create({
-      location: vector.create(utils.random(0, 1500)),
-      acceleration: vector.create(utils.random(0, 585))
-    }))
+      acceleration: vector.create(utils.random(-speed, speed), utils.random(-speed, speed)),
+      location: vector.create(utils.random(0, width), utils.random(0, height)),
+      width: utils.random(1, 3),
+      rotation: parseInt(utils.random(0, 180)),
+      rotationVelocity: utils.random(0, 10),
+    }));
   }
   return particleArray;
 }
 
+function draw(context, particle) {
+  var maxAlpha = .5
+  var loc = particle.position();
+  var alpha = maxAlpha*(loc.x/width);
+  //console.log('rotation ', particle.rotation);
+  // context.save();
+  // context.translate(loc.x,loc.y); 
+  // context.rotate(particle.rotation * Math.PI / 180);
+  context.fillStyle = 'rgba(187, 217, 194, '+alpha+')';
+  context.beginPath();
+  context.arc(loc.x, loc.y, particle.width, 0, 2 * Math.PI);
+  context.closePath();
+  context.fill();
+  // context.translate(0,0); 
+  // context.restore();
+}
+
 module.exports = {
+  /*
+   * Creates the particle system
+   * @params {canvas} el the canvas element
+  */
   create: function(el) {
     var context;
-    //var particles = generateParticles(100);
-
-    el.setAttribute('width', 1500);
-    el.setAttribute('height', 585);
+    var particles = generateParticles(100);
+    var loc;
+    el.setAttribute('width', width);
+    el.setAttribute('height', height);
     context = el.getContext('2d');
-    //background = backgroundFactory.create(context, '/imgs/bkg.jpg');
 
-    
-    function animate(highResTimestamp) {
-      context.clearRect(0, 0, el.width, el.height);
-      // particles.forEach(function(particle) {
-      //   particle.update();
-      //   var loc = particle.position();
-      //   console.log('loc: ', loc)
-      //   context.fillRect(loc.x,loc.y,20,20);
-      // })
+    /*
+     * Animation loop
+    */
+    function animate() {
+      context.clearRect(0, 0, width, height);
+      particles.forEach(function(particle) {
+        loc = particle.position();
+        if(loc.x > width || loc.x < 0) particle.reverse('x')
+        if(loc.y > height || loc.y < 0) particle.reverse('y')
+        particle.update();
+        draw(context, particle);
+      })
 
-      //requestAnimationFrame(animate);
+      requestAnimationFrame(animate);
     }
 
     // Start the animation.
-    //requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
   }
 }
--- src/js/particles/particle.js
+++ src/js/particles/particle.js
@@ -3,20 +3,33 @@ var utils = require('../utils');
 
 defaults = {
   acceleration: vector.create(1,1),
-  location: vector.create(0,0)
+  location: vector.create(0,0), 
+  rotation: 0,
+  rotationVelocity: 0,
+  width: 2
 }
 
 module.exports = {
   create: function(opts) {
-    var settings = utils.merge(opts || {}, defaults);
+
     return {
       update: function() {
-        settings.location.x += settings.acceleration.x
-        settings.location.y += settings.acceleration.y
+        opts.location.x += opts.acceleration.x;
+        opts.location.y += opts.acceleration.y;
+        opts.rotation += opts.rotationVelocity;
       },
       position: function() {
-        return vector.create(settings.location.x, settings.location.y);
-      }
+        return vector.create(opts.location.x, opts.location.y);
+      },
+
+      reverse: function(prop) {
+        if(prop === 'x') opts.acceleration.x *= -1;
+        if(prop === 'y') opts.acceleration.y *= -1
+      },
+
+      width: opts.width,
+
+      rotation: opts.rotation
     }
   }
 }
